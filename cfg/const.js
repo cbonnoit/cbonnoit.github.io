0045656bd43d20dd6d825a4c669f419dc689e01e
Craig Bonnoit <cbonnoit@gmail.com>
2022-05-27 17:27:11 -0400

    Factor logic into classes and libraries

---
+++ cfg/const.js
@@ -0,0 +1,7 @@
+export const SEC_TO_MS = 1000
+export const MIN_TO_MS = 60 * SEC_TO_MS
+export const HOUR_TO_MS = 60 * MIN_TO_MS
+
+export const MS_TO_SEC = 1 / SEC_TO_MS
+export const MS_TO_MIN = 1 / MIN_TO_MS
+export const MS_TO_HOUR = 1 / HOUR_TO_MS
---
+++ cfg/server.js
@@ -0,0 +1,7 @@
+// host name
+export const HOST_NAME = 'd228-108-21-57-184.ngrok.io'
+
+// endpoints - keep in sync with server implementation
+export const SUBSCRIBE_TRANSCRIPT_ENDPOINT = 'subscribe-transcript'
+export const SUBSCRIBE_COACHING_ENDPOINT = 'subscribe-coaching'
+export const PARTY_INFO_ENDPOINT = 'list-party'
