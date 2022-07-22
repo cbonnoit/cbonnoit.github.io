09ae9c9740766b88728e8d4a8a576d3e3bfa8ec5
Ajinkya Nene <ajinkya10@gmail.com>
2022-07-22 16:25:46 -0400

    enables iframe for trellus extension
    latest
    
    nits
    
    nits

---
+++ pages/coaching/index.js
@@ -0,0 +1,26 @@
+import {CoachingManager} from "../../lib/app/coaching-manager.js";
+import {DISABLED} from "../../cfg/const.js";
+
+function onPageLoad () {
+    window.addEventListener("message", receiveMessage, false);
+    console.log('requesting session information')
+    window.parent.postMessage({"type": "getSessionInformation"}, '*');
+}
+
+function receiveMessage (event) {
+    if (event.origin === "https://app.salesloft.com") {
+        const data = event.data
+        const procedure = data['type']
+        if (procedure === 'sessionInformation') {
+            const session = data['session']
+            window._coachingSession = new CoachingManager(session)
+        } else if (procedure === 'stopDrag') {
+            document.querySelector('.trellus-transcript-box').classList.remove(DISABLED)
+            document.querySelector('.trellus-main').classList.remove(DISABLED)
+            document.querySelector('.text-cover').remove()
+        }
+    }
+}
+
+window.addEventListener("load", () => onPageLoad())
+
--- pages/live/index.js
+++
@@ -1,9 +0,0 @@
-import { SessionManager } from "../../lib/app/session-manager.js"
-
-
-function onPageLoad () {
-    window._session = new SessionManager()
-}
-
-
-window.addEventListener("load", () => onPageLoad())
