7ec6103d1f9350e75f9fc104825876b5c279c503
Ajinkya Nene <ajinkya10@gmail.com>
2022-07-22 16:25:46 -0400

    enables iframe for trellus extension
    latest
    
    nits
    
    nits

---
+++ cfg/coach.js
@@ -0,0 +1,42 @@
+export const SETTINGS = Object.freeze({
+    BEHAVIORAL_COACHING: 'Behavioral coaching',
+    PROGRESS_BAR: 'Progress bar',
+    SUGGESTIONS: 'Suggestions',
+    TALK_TIMELINE: 'Talk timeline',
+});
+
+export const SETTING_TO_TARGET_ELEMENT_MAP = Object.freeze({
+    [SETTINGS.BEHAVIORAL_COACHING]: '#behavioral',
+    [SETTINGS.PROGRESS_BAR]: '#progressBarContainer',
+    [SETTINGS.SUGGESTIONS]: '#checklist',
+    [SETTINGS.TALK_TIMELINE]: '#trellus-summary',
+});
+
+export const PROMPT_TYPES = Object.freeze({
+    SUMMARY: 'SUMMARY',
+    MONOLOG: 'MONOLOG',
+    NEXT_STEPS: 'NEXT_STEPS',
+    CADENCE_FAST: 'CADENCE_FAST',
+    SEQUENCE: 'SEQUENCE',
+    OBJECTION_RESPONSE: 'OBJECTION_RESPONSE'
+})
+
+export const BEHAVIORAL_PROMPTS = [PROMPT_TYPES.MONOLOG, PROMPT_TYPES.NEXT_STEPS, PROMPT_TYPES.CADENCE_FAST]
+
+export const SETTING_TO_ONLY_DEMO_MODE = Object.freeze({
+    [SETTINGS.BEHAVIORAL_COACHING]: false,
+    [SETTINGS.PROGRESS_BAR]: true,
+    [SETTINGS.SUGGESTIONS]: false,
+    [SETTINGS.TALK_TIMELINE]: false,
+})
+
+export const PROMPTS_TO_ONLY_DEMO_MODE = Object.freeze({
+    SUMMARY: false,
+    MONOLOG: false,
+    NEXT_STEPS: false,
+    CADENCE_FAST: false,
+    SEQUENCE: true,
+    OBJECTION_RESPONSE: false
+})
+
+export const DEMO_MODE = false
--- cfg/const.js
+++ cfg/const.js
@@ -1,10 +1,19 @@
-export const SEC_TO_MS = 1000
-export const MIN_TO_MS = 60 * SEC_TO_MS
-export const HOUR_TO_MS = 60 * MIN_TO_MS
+// time constants
+export const SEC_TO_MS = 1000;
+export const MIN_TO_MS = 60 * SEC_TO_MS;
+export const HOUR_TO_MS = 60 * MIN_TO_MS;
+export const DAY_TO_MS = 24 * HOUR_TO_MS;
+export const MS_TO_SEC = 1 / SEC_TO_MS;
+export const MS_TO_MIN = 1 / MIN_TO_MS;
+export const MS_TO_HOUR = 1 / HOUR_TO_MS;
+export const MS_TO_DAY = 1 / DAY_TO_MS;
+export const MIN_TO_SEC = MIN_TO_MS * MS_TO_SEC;
+export const SEC_TO_MIN = SEC_TO_MS * MS_TO_MIN;
+export const HOUR_TO_SEC = HOUR_TO_MS * MS_TO_SEC;
+export const SEC_TO_HOUR = SEC_TO_MS * MS_TO_HOUR;
+export const DAY_TO_SEC = DAY_TO_MS * MS_TO_SEC;
+export const SEC_TO_DAY = SEC_TO_MS * MS_TO_DAY;
 
-export const MS_TO_SEC = 1 / SEC_TO_MS
-export const MS_TO_MIN = 1 / MIN_TO_MS
-export const MS_TO_HOUR = 1 / HOUR_TO_MS
-
-export const MIN_TO_SEC = MIN_TO_MS * MS_TO_SEC
-export const SEC_TO_MIN = SEC_TO_MS * MS_TO_MIN
+// named constants for HTML
+export const CLOSED = 'closed';
+export const DISABLED = 'disabled';
