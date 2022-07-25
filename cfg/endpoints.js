2a9ec8e3b0fbfed5624345651a8b7f38c3c4290a
Ajinkya Nene <ajinkya10@gmail.com>
2022-07-25 13:52:15 -0400

    nits

--- cfg/endpoints.js
+++ cfg/endpoints.js
@@ -1,20 +1,6 @@
 // server host url
 export const FORCE_REALTIME_HOSTNAME = null  // set to a specific hostname for local debugging
-export const SERVICE_HOSTNAME = 'api.trellus.ai'
-
-// form pages
-export const SIGNUP_PAGE = `https://app.trellus.ai/pages/signup`
-export const CREATE_SESSION_PAGE = `https://app.trellus.ai/pages/create-session`
-export const UNINSTALL_PAGE = 'https://6d142b93t9r.typeform.com/to/fCoRsTkq'
 
 // realtime endpoints
 export const SUBSCRIBE_TRANSCRIPT_ENDPOINT = 'subscribe-transcript';
 export const SUBSCRIBE_COACHING_ENDPOINT = 'subscribe-coaching';
-export const AUDIO_ENDPOINT = 'audio'
-
-// service endpoints
-export const CHECK_AUTHORIZATION_ENDPOINT = 'check-authorization';
-export const SIGNUP_USER_ENDPOINT = 'signup-user'
-export const FIND_SESSION_ENDPOINT = 'find-session';
-export const CREATE_SESSION_ENDPOINT = 'create-session';
-export const PARTY_INFO_ENDPOINT = 'list-party';
