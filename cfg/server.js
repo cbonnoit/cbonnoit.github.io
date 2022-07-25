674f538fe84095b454ea73b7c1a60002608f76c0
Craig Bonnoit <cbonnoit@gmail.com>
2022-07-25 16:37:06 -0400

    Use names from backend, depricate Namer

--- cfg/server.js
+++
@@ -1,4 +0,0 @@
-// endpoints - keep in sync with server implementation
-export const SUBSCRIBE_TRANSCRIPT_ENDPOINT = 'subscribe-transcript'
-export const SUBSCRIBE_COACHING_ENDPOINT = 'subscribe-coaching'
-export const PARTY_INFO_ENDPOINT = 'list-party'
