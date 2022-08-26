3f8f909f95c7544bbae45632b9a0f0394e925356
Craig Bonnoit <cbonnoit@gmail.com>
2022-08-26 10:04:38 -0400

    Add triggers page

--- cfg/endpoints.js
+++ cfg/endpoints.js
@@ -1,10 +1,13 @@
-// server host url
+// production services hostname
 export const SERVICE_HOSTNAME = 'api.trellus.ai'
 
 // realtime endpoints
 export const SUBSCRIBE_CLIENT_ENDPOINT = 'subscribe-client';
 
+// services endpoints
 export const SIGNUP_USER_ENDPOINT = 'signup-user'
+export const GET_USER_TRIGGERS_ENDPOINT = 'get-user-triggers'
+export const SAVE_USER_TRIGGERS_ENDPOINT = 'save-user-triggers'
 
 // extension ID
 export const EXTENSION_ID = "enhpjjojmnlnaokmppkkifgaonfojigl"
