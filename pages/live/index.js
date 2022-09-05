4342bcde4f03d7f4b4a99a853b60982429733e82
Ajinkya Nene <ajinkya10@gmail.com>
2022-09-05 09:34:00 -0400

    working version all components
    working transcript
    
    working version all
    
    update

--- pages/live/index.js
+++ pages/live/index.js
@@ -1,14 +1,29 @@
+// @ts-check
 import { EXTENSION_ID, SUBSCRIBE_CLIENT_ENDPOINT } from "../../cfg/endpoints.js";
 import { MESSAGE_TYPES } from "../../cfg/messages.js";
-import { logInfo } from "../../lib/core.js";
+import { MIN_TO_SEC, SEC_TO_MS } from "../../cfg/const.js"
+import { BEHAVIORAL_PROMPTS, BEHAVIORAL_PROMPTS_TO_IMAGE,
+  PROMPT_TYPES, PROMPTS_TO_ONLY_DEMO_MODE } from '../../cfg/coach.js';
+import { logInfo, back } from "../../lib/core.js";
 import { createLoggingWebsocketPromise } from "../../lib/network.js";
+import { generateColor } from '../../lib/graphics.js'
+import { createNode, deleteAllChildren } from "../../lib/user-agent.js";
+import { durationToString } from "../../lib/time-fns.js"
 
 const _LOG_SCOPE = '[Trellus][External page]'
 
 let _session = null
 let _socketPromise = null
 let _extensionId = EXTENSION_ID
+let _partyCodeToColor = {}
+let _transcripts = []
+let partyToSummary = new Map()
 
+// keep track of the current shown behavioral prompt
+let activePrompt = null
+let activePromptIntervalId = null
+let dismissObjectionIntervalId = null
+let promptPersistanceDuration = SEC_TO_MS * 5
 
 /**
  * Main entry point to setting up javascript after page is loaded
@@ -92,13 +107,13 @@ async function _createClientSocket(session) {
   const socketUrl = `wss://${session['realtime_hostname']}/${SUBSCRIBE_CLIENT_ENDPOINT}?${queryString}`
   const callback = (dataString) => {
     const data = JSON.parse(dataString)
-    logInfo('Receving data from client socket', JSON.stringify(data))
+    logInfo(`${_LOG_SCOPE} Receving data from client socket`, JSON.stringify(data))
     if (data['transcript_data'] != null) 
       _updateTranscript(data['transcript_data'])
     else if (data['coaching_data'] != null)
       _updateCoachingData(data['coaching_data'])
     else if (data['weather_data'] != null)
-      _updateWeather(data['weather_data'])
+      _updateWeather(data['weather_data']['weather']) // todo: weather by party...
   }
   return await createLoggingWebsocketPromise('clientSocket', socketUrl, callback, false)
 }
@@ -108,23 +123,329 @@ async function _createClientSocket(session) {
  * @param {Object} transcript Transcript object to add
  */
 function _updateTranscript (transcript) {
-  logInfo('got transcript')
+  logInfo(`${_LOG_SCOPE} Transcript from ${transcript['person_name']} (${transcript['party_code']}). Text: "${transcript['text']}"`)
+  // get party code and name
+  const partyCode = transcript['party_code']
+
+  // if new party ID - generate a color for that party
+  if (!(partyCode in _partyCodeToColor))
+    _partyCodeToColor[partyCode] = generateColor(Object.keys(_partyCodeToColor).length)
+
+  // get the text
+  const text = transcript['text']
+
+  // nothing to do if there is no text
+  if (text === '') return
+
+  const root = document.querySelector('#transcriptRoot')
+  const isScrolledToBottom = Math.abs((root.scrollTop + root.offsetHeight) - root.scrollHeight) < 5
+
+  // if this party is also the party of the last transcript, then append the text
+  if ((back(_transcripts) ?? {})['partyCode'] === partyCode)
+    _extendLastTranscript(text)
+  else {
+    // get the start time (relative to audio zero)
+    const startTimeMs = transcript['start_time'] * SEC_TO_MS
+    // create a new transcript
+    _addNewTranscript(text, partyCode, transcript['person_name'], startTimeMs)
+  }
+
+  // keep scroll at bottom. note: if this is done immediately, layout may not be complete
+  if (isScrolledToBottom) setTimeout(() => root.scrollTop = root.scrollHeight, 0)
 }
+
+/**
+ * Extend the last transcript with additional words
+ * @param {String} text Text to extend transcript with
+ */
+function _extendLastTranscript (text) {
+  const transcript = back(_transcripts)
+  transcript.contentNode.textContent += ' ' + text
+}
+
+/**
+ * Create new dom nodes associated with a new transcript
+ * @param {String} text
+ * @param {Number} partyCode
+ * @param {String} partyName
+ * @param {Number} startTime Start time (in milliseconds)
+ */
+function _addNewTranscript (text, partyCode, partyName, startTime) {
+  const root = document.querySelector('#transcriptRoot')
+  // create nodes
+  const transcriptRoot = root.appendChild(createNode('div', {'class': 'flexRowLeft transcript'}))
+  transcriptRoot.appendChild(createNode('div', {'class': 'flexColumn transcriptTime'}, durationToString(startTime)))
+  const nameContentNode = transcriptRoot.appendChild(createNode('div', {'class': 'transcriptNameContent'}))
+  const nameAttributes = {'class': 'transcriptName', 'style': `color: ${_partyCodeToColor[partyCode]};`}
+  nameContentNode.appendChild(createNode('div', nameAttributes, partyName))
+  const contentNode = nameContentNode.appendChild(createNode('div', {'class': 'transcriptContent'}, text))
+
+  // store transcript information
+  _transcripts.push({'partyCode': partyCode, 'contentNode': contentNode})
+}
+
 
 /**
  * Update coaching based on input
  * @param {object} coaching
  */
 function _updateCoachingData (coaching) { 
-  logInfo('got coaching')
+  // starting with 9ce9a73b6d the backend started sending coaching data through the 'coaching' parameter
+  if (coaching.hasOwnProperty('coaching'))
+    coaching = coaching['coaching']
+  
+  const prompt = coaching['prompt'].toUpperCase()
+
+  // per-prompt rendering
+  if (prompt === PROMPT_TYPES.SUMMARY_V2) {
+    _updateSummary(coaching['value'])
+  } else if (prompt === PROMPT_TYPES.OBJECTION_RESPONSE) {
+    _updateTrigger(coaching['value']['objection'], coaching['value']['response'])
+  } else if (prompt === PROMPT_TYPES.TRIGGER) {
+    _updateTrigger(coaching['value']['trigger_name'], coaching['value']['trigger_prompt'])
+  }else if (BEHAVIORAL_PROMPTS.includes(prompt)) {
+    _updateBehavioralSuggestion(prompt)
+  } else if (prompt === PROMPT_TYPES.BUYING_INTENT) {
+    _updateBuyingIntent(coaching['value'])
+  } else {
+    console.log(`[Trellus][iframe] Unknown prompt ${prompt}`)
+  }
 }
+
+
+/**
+ * Updates objection data with new data
+ * @param {string} triggerName trigger name
+ * @param {string} triggerPrompt trigger prompt
+ */
+function _updateTrigger (triggerName, triggerPrompt) {
+  // cancel any existing objection callback
+  if (dismissObjectionIntervalId != null)
+    clearInterval(dismissObjectionIntervalId)
+
+  // set objection text
+  const placeholderText = document.querySelector('#objection-placeholder-text')
+  if (placeholderText) placeholderText.remove()
+
+  document.querySelector('#objection-header').textContent = `Objection: ${triggerName.toLowerCase()}`
+  const objectionContainer = document.querySelector('#objection-container')
+  deleteAllChildren(objectionContainer)
+  const responses = Array.isArray(triggerPrompt) ? triggerPrompt : triggerPrompt.split('\n')
+  for (const response of responses) {
+    objectionContainer.appendChild(_createCheckListElement(response, false))
+  }
+
+  // add a callback dismissing the objection
+  dismissObjectionIntervalId = setTimeout(() => {
+    objectionContainer.innerHTML = ''
+    document.querySelector('#objection-header').textContent = 'Objections'
+    document.querySelector('#objection').appendChild(createNode('div', {id: 'objection-placeholder-text', class: 'placeholder-text'}, "Listening for objections..."))
+  }, promptPersistanceDuration * 2)
+}
+
+/**
+ * Creates a checklist element
+ * @param labelText text for checklist element
+ * @param checked whether the element should already be checked
+ * @returns {HTMLDivElement}
+ */
+function _createCheckListElement(labelText, checked) {
+  const checklistItem = createNode('div', {'class': 'trellus-guidance-checklist-item'})
+  const checkbox = createNode('div', {'class': 'checklist-checkbox'})
+  const checklistItemText = createNode('div', {'class': 'checklist-text'}, labelText)
+
+  if (checked === true) {
+    checkbox.classList.add("checked")
+    checklistItemText.classList.add("checked")
+  }
+
+  checklistItem.appendChild(checkbox)
+  checklistItem.appendChild(checklistItemText)
+  return checklistItem;
+}
+
+/**
+ * Update the checklist with new data
+ * @param {Object} data Sequence data value
+ */
+function _updateChecklist (data) {
+  const checklist = document.querySelector('#checklist')
+  checklist.innerHTML = "";
+  const checklistItems = data['steps']
+  for (const item of checklistItems) {
+    checklist.appendChild(_createCheckListElement(item.label, item.checked))
+  }
+
+  //update the current stage and the next stage
+  const percentCompleted = data['percent_completed']; // comes in as a float [0, 100].
+  const currentStageText = data['current_stage'];
+  const nextStageText = data['next_stage'];
+
+  const progressBar = document.querySelector('#progress-bar');
+  const currentStageDiv = document.querySelector('#current-stage');
+  const nextStageDiv = document.querySelector('#next-stage');
+
+  progressBar.style.width = percentCompleted.toFixed() + '%'; // need to convert to a string
+  currentStageDiv.textContent = 'Current: ' + currentStageText;
+  if (nextStageText !== '') {
+    nextStageDiv.textContent = 'Up Next: ' + nextStageText;
+  } else {
+    nextStageDiv.textContent = ''; // ensures old data gets cleaned up
+  }
+}
+
+function _clearBehavioralSuggestion() {
+  // show the prompt
+  const behavioralImg = document.querySelector('#behavioral-guidance-img')
+  behavioralImg.style.opacity = '0'
+  behavioralImg.src = '/images/great_work.png'
+  behavioralImg.style.opacity = '100%'
+}
+
+/**
+ * Updates the behavioral suggestion with a new prompt
+ * @param {String} prompt
+ */
+function _updateBehavioralSuggestion (prompt) {
+  // if there is an active prompt and the new prompt is different, ignore the new prompt
+  // the assumption here is that prompts will get refreshed as long as they are valid for
+  // and the font end should avoid flickering between multiple simultaneously valid prompts
+  if (activePrompt != null && activePrompt !== prompt) return;
+
+  // clear any pending interruptions
+  if (activePromptIntervalId != null) {
+    clearInterval(activePromptIntervalId);
+  }
+
+  if (!BEHAVIORAL_PROMPTS_TO_IMAGE.hasOwnProperty(prompt)) {
+    console.log(`image not created for prompt: ${prompt}`)
+    return
+  }
+
+  const behavioralImg = document.querySelector('#behavioral-guidance-img')
+  behavioralImg.style.opacity = '0'
+  behavioralImg.src = BEHAVIORAL_PROMPTS_TO_IMAGE[prompt]
+  behavioralImg.style.opacity = '100%'
+
+  activePrompt = prompt;
+
+  // automatically hide the prompt
+  activePromptIntervalId = setTimeout(() => {
+    _clearBehavioralSuggestion()
+    activePrompt = null;
+    activePromptIntervalId = null;
+  }, promptPersistanceDuration)
+}
+
+/**
+ * Update the summary
+ * @param {Array.<Object>} data Summary data
+ */
+function _updateSummary (data) {
+  if (partyToSummary.size === 0) {
+    console.log('[Trellus][Iframe] Sending coaching loaded to coaching UI')
+    window.parent.postMessage({"type": "coachingLoaded"}, '*'); // only load on receiving first summary
+  }
+
+  let totalTalkTime = 0
+  data.forEach((partyData) => {
+    partyData['intervals'].forEach(({start, end}) => {
+      totalTalkTime += end - start
+    })    
+  })
+
+  for (const partyData of data)
+    _updateActiveIntervals(partyData['party_code'], partyData['party_name'], partyData['intervals'], totalTalkTime)
+}
+
+/**
+ * Update intervals by party
+ * @param {Number} partyCode Party identifier to update intervals for
+ * @param {String} partyName Party name to display with intervals
+ * @param {Array.<Object>} intervals intervals in the form {start, end}
+ */
+function _updateActiveIntervals (partyCode, partyName, intervals, totalTalkTime) {
+  // create the party summary
+  if (!partyToSummary.has(partyCode))
+    _createPartySummary(partyCode, partyName)
+
+  // update party names. note there can be a race condition in the coaching run loop between receipt of
+  // audio data and party person data, so it is possible to get a transitive `Unknown` name
+  partyToSummary.get(partyCode).parentElement.querySelector('.summaryName').textContent = partyName
+
+  // remove all previous lines
+  const container = partyToSummary.get(partyCode).querySelector('.summaryIntervalContainer')
+  deleteAllChildren(container)
+
+  // get scale converting between time (in sec) and pixel
+  const width = container.getBoundingClientRect().width
+  const maxTime = 10 * MIN_TO_SEC
+  const scale = width / maxTime
+
+  let partyTalkTime = 0
+
+  // draw intervals
+  intervals.forEach(({start, end}) => {
+    const style = `left: ${start * scale}px; width: ${(end - start) * scale}px`
+    container.appendChild(createNode('div', {'style': style}))
+    partyTalkTime += end - start
+  })
+
+  const partyTalkPercentage = Math.round((partyTalkTime / totalTalkTime) * 100)
+  partyToSummary.get(partyCode).parentElement.querySelector('.summaryPercentage').textContent = partyTalkPercentage + '%'
+}
+
+/**
+ * Create DOM elements for a party summary for `partyCode`
+ * @param {Number} partyCode Party identifier to update intervals for
+ * @param {String} partyName Party name to display with intervals
+ */
+function _createPartySummary (partyCode, partyName) {
+  // create layout
+  const root = document.querySelector('#trellus-summary')
+  const summary = root.appendChild(createNode('div', {'class': 'flexRow full summary'}))
+  summary.appendChild(createNode('div', {'class': 'flexRow full summaryName'}, partyName))
+  const payloadContainer = summary.appendChild(createNode('div', {'class': 'flexRow full summaryRight', 'style': 'position: relative'}))
+  payloadContainer.appendChild(createNode('div', {'class': 'flexRow full summaryTop summaryIntervalContainer'}))
+  payloadContainer.appendChild(createNode('div', {'class': 'flexRow full summaryBottom summaryEventContainer'}))
+  summary.appendChild(createNode('div', {'class': 'flexRow full summaryPercentage'}, partyName))
+
+  // keep a reference to the payload container
+  partyToSummary.set(partyCode, payloadContainer)
+}
+
+function _updateBuyingIntent(data) {
+  const buyingSignalMeter = document.querySelector('#buyingSignal')
+  const percentageSignal = parseFloat(data)*100
+  buyingSignalMeter.setAttribute('aria-valuenow', `${percentageSignal}`)
+  buyingSignalMeter.style.setProperty('--value', `${percentageSignal}`)
+}
+
 
 /**
  * Update weatheer based on input
  * @param {object} weather
  */
  function _updateWeather (weather) { 
-  logInfo('got weather')
+  const weatherElement = document.querySelector('#weather-main')
+
+  const parentDiv = createNode('div', {'style': 'flex-direction: column; display: flex'})
+
+  const upperDiv = createNode('div', {'style': 'display: flex; flex-wrap: nowrap; align-items: center; justify-content: center;'})
+  const weatherValue = createNode('div', {}, Math.trunc(weather['temperature']) + String.fromCharCode(176) + 'F')    
+  const weatherImg = createNode('img', {'src': weather['icon_url'], 'style': 'background-color: #adc1e67d; border-radius: 50%; margin-left: 10px;'})
+  upperDiv.appendChild(weatherValue)
+  upperDiv.appendChild(weatherImg)
+
+  const bottomDiv = createNode('div')
+  const weatherLocation = createNode('div', {'style': 'margin-top: 4px; color: #423838;'}, weather['location_str'])
+  bottomDiv.appendChild(weatherLocation)
+
+  parentDiv.appendChild(upperDiv)
+  parentDiv.appendChild(bottomDiv)
+
+  weatherElement.innerHTML = '';
+  weatherElement.appendChild(parentDiv)
 }
 
 window.addEventListener("load", () => onPageLoad())
