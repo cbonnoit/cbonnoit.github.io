b3b6454fac1023a5e61765bf8389002dc50c9883
Craig Bonnoit <cbonnoit@gmail.com>
2022-09-08 08:52:15 -0400

    Update history page

--- cfg/const.js
+++ cfg/const.js
@@ -13,7 +13,9 @@ export const HOUR_TO_SEC = HOUR_TO_MS * MS_TO_SEC;
 export const SEC_TO_HOUR = SEC_TO_MS * MS_TO_HOUR;
 export const DAY_TO_SEC = DAY_TO_MS * MS_TO_SEC;
 export const SEC_TO_DAY = SEC_TO_MS * MS_TO_DAY;
-export const MICRO_TO_SEC = 1 / 1000000
+export const MS_TO_MICRO = 1000;
+export const MICRO_TO_MS = 1 / MS_TO_MICRO;
+
 
 // named constants for HTML
 export const CLOSED = 'closed';
--- pages/history/index.js
+++ pages/history/index.js
@@ -1,26 +1,21 @@
-import { MICRO_TO_SEC } from "../../cfg/const.js"
+import { MICRO_TO_MS } from "../../cfg/const.js"
 import { getListConversations } from "../../lib/app/services.js"
-import { back } from "../../lib/core.js"
+import { back, logInfo } from "../../lib/core.js"
 import { durationToString } from "../../lib/time-fns.js"
 import { createNode, deleteAllChildren } from "../../lib/user-agent.js"
 
 // define constants
-let _API_KEY = null
-let _FORCE_SERVICES_HOSTNAME = null
+let _API_KEY = (new URLSearchParams(window.location.search)).get('apiKey')
+let _FORCE_SERVICES_HOSTNAME = (new URLSearchParams(window.location.search)).get('forceServicesName')
+let _INITILIZED = false
+
+const _LOG_SCOPE = `[Trellus][History page]`
 
 // initialize page
-initialize()
-
-
-function initialize () {
-  // start listener
-  listenExtensionInformation()
-
-  // bind button clicks
-  document.querySelector('#add').addEventListener('click', () => appendTrigger('', false, '', ['']))
-  document.querySelector('#save').addEventListener('click', () => saveTriggers())
-
-  console.log('Page initialized')
+listenExtensionInformation()
+if (_API_KEY != null && !_INITILIZED) {
+  _INITILIZED = true
+  loadHistory()
 }
 
 /**
@@ -32,7 +27,10 @@ function listenExtensionInformation () {
     if (messageType === 'APP_TO_EXTERNAL_SET_EXTENSION_INFO') {
         _API_KEY = event.data['apiKey']
         _FORCE_SERVICES_HOSTNAME = event.data['forceServicesHostname']
-        loadHistory()
+        if (!_INITILIZED) {
+          _INITILIZED = true
+          loadHistory()
+        }
     }
   })
 }
@@ -42,28 +40,34 @@ function listenExtensionInformation () {
  * @param {Number|null} maxStartMicroSec
  */
 async function loadHistory (maxStartMicroSec=null) {
-    // get data
-    const conversations = await getListConversations(_API_KEY, maxStartMicroSec, _FORCE_SERVICES_HOSTNAME)
-    
-    // clear the table
-    const table = document.querySelector('tbody')
-    deleteAllChildren(table)
+  // get data
+  const conversations = await getListConversations(_API_KEY, maxStartMicroSec, _FORCE_SERVICES_HOSTNAME)
+  
+  // clear the table
+  const table = document.querySelector('tbody')
+  deleteAllChildren(table)
 
-    // add new rows for each conversation
-    conversations.forEach((x) => {
-        const startTime = new Date(x['start'] * MICRO_TO_SEC)
-        const duration = (x['end'] - x['start']) * MICRO_TO_SEC
-        let name = x['person_name']
-        if (x['num_other_people'] > 1) name += ` + ${x['num_other_people']} others`
-        const row = conversations.appendChild(createNode('tr', {'data-session-id': x['session_id']}))
-        row.appendChild(createNode('td', null, startTime.toISOString()))
-        row.appendChild(createNode('td', null, durationToString(duration)))
-        row.appendChild(createNode('td', null, name))
-        const detailsNode = row.appendChild(createNode('td'))
-        detailsNode.appendChild(createNode('a', {'href': `./session/${x['session_id']}}`}))
-    })
+  // make a date formater
+  const formater = new Intl.DateTimeFormat("en" , {
+    month: 'short', day: 'numeric', hour: "numeric", minute: '2-digit', hour12: true,
+  });
 
-    // save the max start on the table
-    if (conversations.length > 0)
-        table.setAttribute('data-max-start-micro', back(conversations)['start'])
+  // add new rows for each conversation
+  conversations.forEach((x) => {
+    const hasAudio = x['start'] != null
+    const startDate = hasAudio ? new Date(x['start'] * MICRO_TO_MS) : null
+    const duration = hasAudio ? (x['end'] - x['start']) * MICRO_TO_MS : 0
+    let name = x['person_name']
+    if (x['num_other_people'] > 1) name += ` + ${x['num_other_people']} others`
+    const row = table.appendChild(createNode('tr', {'data-session-id': x['session_id']}))
+    row.appendChild(createNode('td', null, formater.format(startDate)))
+    row.appendChild(createNode('td', null, durationToString(duration)))
+    row.appendChild(createNode('td', null, name))
+    const detailsNode = row.appendChild(createNode('td'))
+    detailsNode.appendChild(createNode('a', {'href': `./session/${x['session_id']}${window.location.search}`}, 'See more'))
+  })
+
+  // save the max start on the table
+  if (conversations.length > 0)
+      table.setAttribute('data-max-start-micro', back(conversations)['start'])
 }
